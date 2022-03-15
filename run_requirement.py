37503ae2b3981fe3bd183677a3066ba6126163dd
Crazy-serial <74966286+Crazy-serial@users.noreply.github.com>
2022-03-15 21:24:02 +0900

    Create run_requirement.py

---
+++ run_requirement.py
@@ -0,0 +1,14 @@
+from os import system
+
+# commands = ["pip install img2pdf", "pip install bs4", "pip install pillow"]
+commands = [
+    "pip install img2pdf",
+    "pip install bs4",
+    "pip install pillow",
+    "pip install selenium",
+    "apt-get update",
+    "apt install chromium-chromedriver",
+    "cp /usr/lib/chromium-browser/chromedriver /usr/bin"
+]
+for run in commands:
+    system(run)
